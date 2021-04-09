65e67ef1846c0f99440bb2b3f4af778061dc541d
Caleb Moses <calebjdmoses@gmail.com>
2021-04-09 13:51:56 +1200

    Run sentence_tokenize on directories

--- papers_past/sentence_tokenize.py
+++ papers_past/sentence_tokenize.py
@@ -1,23 +1,37 @@
+import os
 import click
 import logging
 from nltk.tokenize import sent_tokenize
 
 
 @click.command()
-@click.option('--filepath', help='Path to xml files')
+@click.option('--doc_dir', help='Path to documents directory')
+@click.option('--out_dir', help='Path to save output')
 @click.option('--log_level', default='INFO', help='Log level (default: INFO)')
-def main(filepath, log_level):
+def main(doc_dir, out_dir, log_level):
 
     # Set logger config
     logging.basicConfig(level=log_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    with open(filepath, 'r') as f:
-        text = f.read()
+    logging.info("Creating directory: {}".format(out_dir))
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
 
-    with open(filepath.replace('.txt', '_sentence_lines.txt'), 'w') as f:
-        for sent in sent_tokenize(text):
-            sent = sent.replace('\n', ' ').strip()
-            f.write(sent + '\n')
+    for root, dirs, files in os.walk(doc_dir):
+        for f in files:
+            if f.endswith('.txt'):
+
+                fp = os.path.join(root, f)
+                out = os.path.join(out_dir, f)
+
+                with open(fp, 'r') as f:
+                    text = f.read()
+
+                logging.debug('Splitting {} into sentences {}'.format(fp, out))
+                with open(out, 'w') as f:
+                    for sent in sent_tokenize(text):
+                        sent = sent.replace('\n', ' ').strip()
+                        f.write(sent + '\n')
 
 
 if __name__ == '__main__':
